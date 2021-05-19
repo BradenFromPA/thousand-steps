14ddef575f9ca7b81d7181ce5522a27bcf6bbaf5
Braden Hancock <bradenfrompa>
2021-05-19 17:40:02 -0400

    added /log route

--- src/app.js
+++ src/app.js
@@ -5,9 +5,13 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000
 
+// Configure Public path
 app.use(express.static(path.join(__dirname, './public')));
 
+
+// Configure Views path
 app.set('views', path.join(__dirname, './views'));
+// Set view engine to Pug
 app.set('view engine', 'pug');
 
 
@@ -15,6 +19,10 @@ app.set('view engine', 'pug');
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+// Log route handler
+const log = require('./routes/log');
+app.use('/log', log);
 
 app.listen(PORT, () => {
     console.log(`Your application is running at http://localhost:${PORT}`);
