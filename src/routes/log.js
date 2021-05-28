e45fc9e340c0835d8a35c4861b126fae07feb661
Braden Hancock <bradenfrompa>
2021-05-28 10:43:50 -0400

    built basic front-end for testing

--- src/routes/log.js
+++ src/routes/log.js
@@ -2,7 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('log/log')
+    res.render('log/log', {
+        name: 'Braden H.',
+        town: 'Mount Union, PA'
+    })
+});
+
+router.get('/signin', (req, res) => {
+    res.render('log/signin/signin')
 });
 
 module.exports = router;
